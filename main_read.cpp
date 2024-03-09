11713a3df51dea47ad45d343d028d9e94d91f7c0
angelourias <angelou02005@gmail.com>
2024-03-08 21:17:20 -0800

    lab

--- main_read.cpp
+++ main_read.cpp
@@ -2,9 +2,34 @@
 #include <fstream>
 using namespace std;
 
-int main()
-{
-	/***************************************************
-	 * Code your program here
-	 ************************************************** */
+int main() {
+    int N;
+
+    ofstream outputFile("students.txt");
+
+    if (!outputFile.is_open()) {
+        cout << "Error: Unable to open the file." << endl;
+        return 1;
+    }
+
+    cout << "Enter the number of students: ";
+    cin >> N;
+
+    outputFile << N << endl;
+
+    for (int i = 0; i < N; ++i) {
+        string name;
+        int score1, score2;
+
+        cout << "Enter name and two scores for student " << (i + 1) << ": ";
+        cin >> name >> score1 >> score2;
+
+        outputFile << name << " " << score1 << " " << score2 << endl;
+    }
+
+    outputFile.close();
+
+    cout << "Student information saved to students.txt" << endl;
+
+    return 0;
 }
--- main_write.cpp
+++ main_write.cpp
@@ -1,10 +1,37 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <iomanip>
 using namespace std;
 
-int main()
-{
-   /***************************************************
-    * Code your program here
-    ************************************************** */
+int main() {
+    ifstream inputFile("students.txt");
+
+    if (!inputFile.is_open()) {
+        cout << "Error: Unable to open the file." << endl;
+        return 1;
+    }
+
+    int N;
+    inputFile >> N;
+
+    for (int i = 0; i < N; ++i) {
+        string name;
+        int score1, score2;
+
+        inputFile >> name >> score1 >> score2;
+
+        int sum = score1 + score2;
+        float avg = sum / 2.0f;
+
+        cout << "Student Name: " << setw(10) << left << name
+             << " score1: " << setw(5) << score1
+             << " score2: " << setw(5) << score2
+             << " Sum: " << setw(5) << sum
+             << " Avg: " << setw(5) << fixed << setprecision(2) << avg << endl;
+    }
+
+    inputFile.close();
+
+    return 0;
 }
